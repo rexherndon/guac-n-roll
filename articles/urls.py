f009a8fe5033a92f832ec9c36cc9cc80a727d716
Rex Herndon <herndonrex1@gmail.com>
2022-11-17 22:15:00 -0600

    Worked on permissions and added comments to posts

--- articles/urls.py
+++ articles/urls.py
@@ -5,15 +5,18 @@ from .views import (
     ArticleDetailView, # new
     ArticleUpdateView, # new
     ArticleDeleteView, # new
+    ArticleCreateView, # new
 )
 
 urlpatterns = [
-    path("<int:pk>/", ArticleDetailView.as_view(),
-        name="article_detail"), # new
-    path("<int:pk>/edit/", ArticleUpdateView.as_view(),
-        name="article_edit"), # new
-    path("<int:pk>/delete/", ArticleDeleteView.as_view(),
-        name="article_delete"), # new
-    path("", ArticleListView.as_view(), 
-        name="article_list"),
+    path("<int:pk>/", 
+        ArticleDetailView.as_view(), name="article_detail"), # new
+    path("<int:pk>/edit/", 
+        ArticleUpdateView.as_view(), name="article_edit"), # new
+    path("<int:pk>/delete/", 
+        ArticleDeleteView.as_view(), name="article_delete"), # new
+    path("", 
+        ArticleListView.as_view(), name="article_list"),
+    path("new/", 
+        ArticleCreateView.as_view(), name="article_new"), # new
 ]
